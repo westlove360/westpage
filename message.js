017dd9f440cbc9103379725dbaf65baf544b7c90
westlove360 <kayodeabayomi709@gmail.com>
2022-07-16 17:56:00 -0700

    files updated

---
+++ message.js
@@ -0,0 +1,42 @@
+    //function msg(){
+      //    alert("Hello javatpoiont");
+       //   alert("dont load this page");
+          
+//}
+function myfuction(){
+var a=8;
+var b=6;
+alert(`the answer is ${a+b}`);
+  let guestlist=`guest:
+                *west
+                *west
+                *west
+                *west
+                *west
+
+                      `;
+                      alert(guestlist);
+                      let c=6;
+                      let d=3;
+                      let e=a>b;
+                      alert(typeof e);
+}
+function dami(){
+let person={"surname": "olu",
+             "firstname":"west",
+             "age":18,
+             "height":25,
+}
+alert(person.surname);
+}
+var west= 1;
+var sum = 0;
+var add=west/ sum;
+alert(add);
+//var a=new Array ("lagos", "ibadan", "london", "ekiti");
+//alert(a[1]);
+//var a=5;
+//var b=2;
+//alert(a%b);
+var a="6", b=6;
+alert(a!=b);
